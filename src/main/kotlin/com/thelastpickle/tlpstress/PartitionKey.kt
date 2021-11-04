795be61f46c8456a7ed7313457a6e9c068aabff5
Jon Haddad <jhaddad@netflix.com>
2021-11-04 11:47:27 -0700

    Add generation timestamp to PrimaryKey

--- src/main/kotlin/com/thelastpickle/tlpstress/PartitionKey.kt
+++ src/main/kotlin/com/thelastpickle/tlpstress/PartitionKey.kt
@@ -1,9 +1,11 @@
 package com.thelastpickle.tlpstress
 
+import java.time.Instant
+
 /**
  * Will replace the current requirement that a PK can only be a text field
  */
-class PartitionKey(val prefix: String, val id: Long) {
+class PartitionKey(val prefix: String, val id: Long, val generationTime: Instant = Instant.now()) {
 
     fun getText(): String {
         return prefix + id.toString()
